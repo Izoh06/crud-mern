8f7e4fa2e89420aacd4e85009c47650fa68dcd78
Izoh06 <isaacmutugi06@gmailcom>
2023-01-29 10:38:21 +0300

    Initial Commit

---
+++ zeros/zerosApp.swift
@@ -0,0 +1,17 @@
+//
+//  zerosApp.swift
+//  zeros
+//
+//  Created by USER on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct zerosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
